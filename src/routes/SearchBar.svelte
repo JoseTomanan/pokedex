2cab000f0ab676a6019dd8a6504c9b56d9793e64
HoweZae <jdtomanan@up.edu.ph>
2026-02-26 19:34:26 +0800

    feat: init search bar; include comments on what's missing

---
+++ src/routes/SearchBar.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import MdiSearch from "~icons/mdi/search";
+	import { Input } from "$lib/components/ui/input/index";
+
+	let searchFilter = "name" // TODO: make this getContext()
+</script>
+
+
+<div class="relative flex flex-row items-center justify-center outline-input px-2.5">
+	<Input id="searchQuery"
+					type="text"
+					// TODO: value bound to contextualized sort
+					class="focus:outline-none focus:ring-0 focus:ring-offset-0 focus-visible:outline-none focus-visible:ring-0 focus-visible:ring-offset-0"
+					// TODO: add filter name to placeholder
+					placeholder={`Search by ${searchFilter}...`} />
+	<MdiSearch class="grow size-4 m-auto text-muted-foreground absolute top-1/2 -translate-1/2 right-4 pointer-events-none" />
+	<!-- TODO: add change filter stuff. maybe until size md lang?-->
+</div>
