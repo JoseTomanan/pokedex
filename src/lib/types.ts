030b4a4d07271f7b0f82367a9724f8291fd831d4
HoweZae <jdtomanan@up.edu.ph>
2026-02-27 10:43:01 +0800

    chore: update types; segregate short (card) from details (dialog)

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,11 +1,19 @@
-export interface SpeciesShort {
-  id: number;
+export interface Resource {
   name: string;
   url: string;
 }
 
-export interface SpeciesLong extends SpeciesShort {
-  gender_rate: number;
-  capture_rate: number;
+export interface SpeciesShort {
+  id: number;
+  name: string;
+  url: string;
+  types: string[];
+  details: SpeciesDetails;
+}
+
+export interface SpeciesDetails {
+  height: number;
+  weight: number;
+  stats: {base_stat: number, effort: number, stat: Resource[]}[]
   // TODO: complete
 }
