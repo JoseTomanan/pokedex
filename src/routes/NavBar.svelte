7b36a958055a206f1154d95bd7debd77ad1cc21e
HoweZae <jdtomanan@up.edu.ph>
2026-02-27 19:38:03 +0800

    feat: add hover animation

--- src/routes/NavBar.svelte
+++ src/routes/NavBar.svelte
@@ -22,16 +22,16 @@
 </script>
 
 <nav class="flex flex-row justify-between items-center">
-  <button class="flex flex-row gap-2 justify-center items-center text-left cursor-pointer"
+  <button class="group flex flex-row gap-2 justify-center items-center text-left cursor-pointer"
         onclick={scrollToTop}>
     <img src={pokeball}
           alt="Logo"
           class="size-8 slow-spin"/>
     <span class="hidden md:flex md:flex-col">
-      <h3 class="-mb-1 font-semibold">
+      <h3 class="-mb-1 font-semibold group-hover:text-primary">
         Oak's Assistant
       </h3>
-      <h5 class="-mt-1">Made for Professor Oak</h5>
+      <h5 class="-mt-1 group-hover:opacity-100">Made for Professor Oak</h5>
     </span>
   </button>
   <div class="space-x-4 tracking-tight header-links">
