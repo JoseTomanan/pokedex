570c29f9a5512d57e9ae592755c588a67b83c806
HoweZae <jdtomanan@up.edu.ph>
2026-02-27 18:43:50 +0800

    feat: add skeleton for image loading

--- src/lib/components/SpeciesCard.svelte
+++ src/lib/components/SpeciesCard.svelte
@@ -1,15 +1,16 @@
 <script lang="ts">
-  import { Trigger } from "./ui/dialog";
-	import { TYPE_BG_COLORS, TYPE_RING_COLORS } from "@/constants";
-
-	import { PokemonType } from "@/enums";
-  import type { SpeciesShort, SpeciesDetails } from "@/types";
-	import { getIdAsParam } from "@/utils";
-
   const { name, id, url, types }: SpeciesShort = $props();
 
-  // svelte-ignore state_referenced_locally
-  const idParam: string = getIdAsParam(id);
+  import { Trigger } from "./ui/dialog";
+  import { Skeleton } from "./ui/skeleton";
+	import { TYPE_BG_COLORS, TYPE_RING_COLORS } from "@/constants";
+
+  import type { SpeciesShort, SpeciesDetails } from "@/types";
+	import { getIdAsParam } from "@/utils";
+	import { onMount } from "svelte";
+
+  const idParam: string = $derived(getIdAsParam(id));
+  const thumbnailImageLink = $derived(`https://assets.pokemon.com/assets/cms2/img/pokedex/full/${idParam}.png`);
 
   const typeIcons = import.meta.glob('$lib/assets/pokemon-types-icons/*.svg', {
     eager: true,
@@ -18,29 +19,38 @@
   });
 
   const getTypeIcon = (t: string) => typeIcons[`/src/lib/assets/pokemon-types-icons/${t}.svg`];
+  const ringColor = $derived(TYPE_RING_COLORS[types[0] as keyof typeof TYPE_RING_COLORS]);
 
-  console.log(getTypeIcon("bug"));
+  let isImageLoading: boolean = $state(false);
+  let imageSrc = $state();
 
-  // svelte-ignore state_referenced_locally
-  const ringColor = TYPE_RING_COLORS[types[0] as keyof typeof TYPE_RING_COLORS];
+  onMount(async () => {
+    isImageLoading = true;
+    const img = new Image();
 
-  // svelte-ignore state_referenced_locally
-  const bgColor = TYPE_BG_COLORS[types[0] as keyof typeof TYPE_BG_COLORS];
+    img.src = thumbnailImageLink;
+    img.onload = () => {
+      imageSrc = thumbnailImageLink;
+      isImageLoading = false;
+    }
+
+    img.onerror = () => isImageLoading = false;
+  });
 </script>
 
 
 <Trigger class={`card group ${ringColor} flex flex-col items-center overflow-clip transition
-                  hover:ring-3 hover:bg`}>
+                  hover:ring-2 hover:bg hover:border-card`}>
   <div class="flex flex-row justify-between items-center gap-1 w-full">
     <h4 class="font-semibold">
       {name.charAt(0).toUpperCase() + name.slice(1)}
     </h4>
-    <h6 class="font-mono z-90 leading-none bg-card text-foreground/90">
+    <h6 class="font-mono z-2 leading-none bg-card/60 text-foreground/90">
       #{idParam}
     </h6>
   </div>
   <div class="flex flex-row justify-between items-end w-full">
-    <div class="flex flex-col gap-0.5">
+    <div class="flex flex-row gap-1">
       {#each types as type}
         {@const thisBgColor = TYPE_BG_COLORS[type as keyof typeof TYPE_BG_COLORS]}
         {@const typeIconUrl = (() => {
@@ -52,14 +62,17 @@
         <span class={`${thisBgColor} p-1 text-foreground rounded-full w-fit opacity-80
                         group-hover:opacity-100`}>
           <img src={typeIconUrl as string}
-                class="size-3"
+                class="size-4"
                 alt={type} />
         </span>
       {/each}
     </div>
-    <img src={`https://assets.pokemon.com/assets/cms2/img/pokedex/full/${idParam}.png`}
-          alt={name}
-          class="size-32 opacity-60 z-10 -mt-6 -mb-8 -ml-8
-                  group-hover:opacity-100"/>
+    {#if isImageLoading}
+      <Skeleton class="rounded-full size-26 bg-muted-foreground/25"/>
+    {:else}
+      <img src={imageSrc as string}
+            alt={name}
+            class="size-40 opacity-60 z-1 -mt-6 -mb-8 -ml-8 group-hover:opacity-100"/>
+    {/if}
   </div>
 </Trigger>
